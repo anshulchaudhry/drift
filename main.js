272ce97df833dddc3a47712e93952ccecb755d34
Anshul Chaudhry <anshulchaudhry@Anshuls-MacBook-Air.local>
2026-08-23 23:13:28 +0530

    min window resizing

--- main.js
+++ main.js
@@ -1,12 +1,30 @@
 const { app, BrowserWindow } = require("electron");
 
+app.setName("Drift");
+
 function createWindow() {
     const mainWindow = new BrowserWindow({
         width: 1200,
         height: 800,
+        minWidth: 800,
+        minHeight: 600,
     });
 
     mainWindow.loadFile("index.html");
 }
 
-app.whenReady().then(createWindow);
+app.whenReady().then(() => {
+    createWindow();
+
+    app.on("activate", () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow();
+        }
+    });
+});
+
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
+        app.quit();
+    }
+});
